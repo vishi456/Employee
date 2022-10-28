7f3fbb23116d0b011b0fa322560d1969ecbd7c1a
vishi456 <vishi.great@gmail.com>
2022-10-28 17:01:06 +0530

    Commit employees

--- src/main/java/com/example/controller/EmpController.java
+++ src/main/java/com/example/controller/EmpController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class EmpController {
     @GetMapping("/")
     public String home(){
-        return "index";
+        return "indexpage";
     }
 }
