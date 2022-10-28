de65c0505c548beedda1ff8a904a7124a2a8c31f
vishi456 <vishi.great@gmail.com>
2022-10-28 16:56:30 +0530

    Commit employee

--- src/main/java/com/example/controller/EmpController.java
+++ src/main/java/com/example/controller/EmpController.java
@@ -1,4 +1,11 @@
 package com.example.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
 public class EmpController {
+    @GetMapping("/")
+    public String home(){
+        return "index";
+    }
 }
